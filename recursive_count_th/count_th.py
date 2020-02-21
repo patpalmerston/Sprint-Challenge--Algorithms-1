52ef896a3a86995492702e357e83974d0186ba20
Pat Palmerston <patpalmerston@gmail.com>
2020-02-21 12:51:55 -0800

    recursive sub string count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,16 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    # if there length of word is less than one return a zero value
+    if len(word) <= 1:
+        return 0
+    # every time the first two indexes equal th
+    if word[0] + word[1] == 'th':
+        # recursively remove the first two indexes and add 1 value every time a new list is returned
+        return count_th(word[1:]) + 1
+    else:
+        # otherwise return the list
+        return count_th(word[1:])
